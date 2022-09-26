1cb927bc232acc0e9391fc00509c9a9f8d7aeaed
bbashcode <binbash.code@gmail.com>
2022-09-26 20:03:08 +0000

    setup repo

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+const App = ()=> {
+  return (
+    <div className="App">
+      <h2>Turnip: Coming up soon....</h2>
+    </div>
+  );
+};
+
+export default App;
