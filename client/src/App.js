a782534a6ebcb9721c6fc1b44a91cd2060e95a8e
BurritoCarlito <carlotanjuaquio@gmail.com>
2022-09-26 20:59:30 -0700

    created/added Navigation component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+
+import Navigation from './components/Navigation';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>Turnip coming up soon!</h3>
-      </header>
+      <Navigation />
     </div>
   );
 };
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Navigation(){
+  return (
+  <h1> I am the navigation component</h1>
+  );
+}
+
+export default Navigation;
+
